be16d6361b6763b0f1ef2ee2877a0b2b384daf36
admin <admin@admins-MBP.local>
2017-11-11 15:31:17 +0100

    login und sign up
    login und sign up

---
+++ Podfile
@@ -0,0 +1,25 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'iOS09' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for iOS09
+
+  target 'iOS09Tests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'iOS09UITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+
+pod 'Firebase/Core'
+
+pod 'Firebase/Auth‘
+
+end
---
+++ iOS09/SignUpViewController.swift
@@ -0,0 +1,66 @@
+//
+//  SignUpViewController.swift
+//  iOS09
+//
+//  Created by admin on 11.11.17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+
+import UIKit
+import Firebase
+import FirebaseAuth
+
+class SignUpViewController: UIViewController {
+    
+    //Outlets
+    @IBOutlet weak var userName: UITextField!
+    @IBOutlet weak var emailTextField: UITextField!
+    @IBOutlet weak var passwordTextField: UITextField!
+    @IBOutlet weak var confirmPassword: UITextField!
+    //Sign Up Action for email
+    
+    @IBAction func createAccountAction(_ sender: Any) {
+        if emailTextField.text == "" {
+            let alertController = UIAlertController(title: "Error", message: "Please enter your email and password", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }
+        if passwordTextField.text != confirmPassword.text{
+            let alertController = UIAlertController(title: "Error", message: "Error! No password match!", preferredStyle: .alert)
+            
+            let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+            alertController.addAction(defaultAction)
+            
+            present(alertController, animated: true, completion: nil)
+            
+        }
+        else {
+            Auth.auth().createUser(withEmail: emailTextField.text!, password: passwordTextField.text!) { (user, error) in
+                
+                if error == nil {
+                    print("You have successfully signed up")
+                    //Goes to the Setup page which lets the user take a photo for their profile picture and also chose a username
+                    
+                    let vc = self.storyboard?.instantiateViewController(withIdentifier: "HomeScreen")
+                    self.present(vc!, animated: true, completion: nil)
+                    
+                } else {
+                    let alertController = UIAlertController(title: "Error", message: error?.localizedDescription, preferredStyle: .alert)
+                    
+                    let defaultAction = UIAlertAction(title: "OK", style: .cancel, handler: nil)
+                    alertController.addAction(defaultAction)
+                    
+                    self.present(alertController, animated: true, completion: nil)
+                }
+            }
+        }
+    }
+    
+    
+}
+
