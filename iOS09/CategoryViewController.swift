d7acddd96608eb12416371334333a0b75f220862
admin <admin@admins-mbp.local>
2017-11-22 16:50:12 +0100

    tap recognizer

--- iOS09/CategoryViewController.swift
+++ iOS09/CategoryViewController.swift
@@ -8,7 +8,7 @@
 import Foundation
 import UIKit
 
-class CategoryViewController: ViewController {
+class CategoryViewController: ViewController, UIGestureRecognizerDelegate{
     
     @IBOutlet weak var tableView: UITableView?
     
@@ -18,11 +18,37 @@ class CategoryViewController: ViewController {
         super.viewDidLoad()
         
         categoryItems = CategoryItemsManager.sharedManager.loadData()
+        
+        let tap = UITapGestureRecognizer(target: self, action: #selector(self.tapEdit(recognizer:)))
+        tableView?.addGestureRecognizer(tap)
+        tap.delegate = self
+        
     }
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
+    
+    @objc func tapEdit(recognizer: UITapGestureRecognizer)  {
+        if recognizer.state == UIGestureRecognizerState.ended {
+            let tapLocation = recognizer.location(in: self.tableView)
+            if let tapIndexPath = self.tableView?.indexPathForRow(at: tapLocation) {
+                if (self.tableView?.cellForRow(at: tapIndexPath) as? CategoryItemTableViewCell) != nil {
+                    //
+                    switch tapIndexPath.row {
+                    case 0: //For "one"
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+                        self.present(vc!, animated: true, completion: nil)
+                    default: //For "three"
+                        let vc = self.storyboard?.instantiateViewController(withIdentifier: "Login")
+                        self.present(vc!, animated: true, completion: nil)
+                    }
+                    
+                }
+            }
+        }
+    }
+    
 }
 // MARK: - UITableViewDataSource
 extension CategoryViewController: UITableViewDataSource {
@@ -43,17 +69,6 @@ extension CategoryViewController: UITableViewDataSource {
     
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categoryItems.count
-    }
-    
-    func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let segueIdentifier: String
-        switch indexPath.row {
-        case 0: //For "one"
-            segueIdentifier = "sportSegue"
-        default: //For "three"
-            segueIdentifier = "danceSegue"
-        }
-        self.performSegue(withIdentifier: segueIdentifier, sender: self)
     }
 
     
