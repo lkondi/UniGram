3aea6b98f5bae18c7dd5f4d74de94af43845b1b5
Lidia <lkondylidou@p200300cf1bd86a0014741959af9d86e4.dip0.t-ipconnect.de>
2017-12-09 14:26:49 +0100

    firsttry

---
+++ iOS09/ScheduleViewController.swift
@@ -0,0 +1,16 @@
+//
+//  ScheduleViewController.swift
+//  iOS09
+//
+//  Created by Lydia Kondylidou on 09.12.17.
+//  Copyright © 2017 admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+import Firebase
+
+class ScheduleTableViewController: UITableViewController {
+    
+}
+
