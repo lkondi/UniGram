690472f7f59e5d38aeefa37d53b4827a8aa26701
Lidia <lkondilidis@lydias-mbp.local>
2018-01-15 17:12:30 +0100

    usercell

---
+++ iOS09/UserCell.swift
@@ -0,0 +1,28 @@
+//
+//  UserCell.swift
+//  iOS09
+//
+//  Created by Lydia Kondylidou on 12.01.18.
+//  Copyright © 2018 admin. All rights reserved.
+//
+
+import Foundation
+import UIKit
+
+class UserCell: UITableViewCell {
+    
+    @IBOutlet weak var emailLabel: UILabel!
+    @IBOutlet weak var button: UIButton!
+    
+    var buttonFunc: (() -> (Void))!
+    
+    @IBAction func buttonTapped(_ sender: UIButton) {
+        buttonFunc()
+    }
+    
+    func setFunction(_ function: @escaping () -> Void) {
+        self.buttonFunc = function
+    }
+    
+}
+
