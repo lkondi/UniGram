0218e67740b9bfccf25c5886fb8a2be7d697f5c5
Eli <elena.terzieva@campus.lmu.de>
2018-01-27 15:04:08 +0100

    jetzt geht es

--- iOS09/CategoryViewController.swift
+++ iOS09/CategoryViewController.swift
@@ -53,5 +53,40 @@ extension CategoryViewController: UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
         return categoryItems.count
     }
+    
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        
+        super.prepare(for: segue, sender: sender)
+        
+        switch(segue.identifier ?? "") {
+            
+        case "ProfileView":
+            os_log("Adding a new event.", log: OSLog.default, type: .debug)
+            
+        case "ScheduleView":
+            os_log("Scheduled events.", log: OSLog.default, type: .debug)
+            
+        case "EventList":
+            
+            guard let selectedEventCell = sender as? CategoryItemTableViewCell else {
+                fatalError("Unexpected sender: \(String(describing: sender))")
+            }
+            
+            guard let indexPath = tableView.indexPath(for: selectedEventCell) else {
+                fatalError("The selected cell is not being displayed by the table")
+            }
+            
+            guard let eventDetailViewController = segue.destination as? EventTableViewController else {
+                fatalError("Unexpected destination: \(segue.destination)")
+                
+            }
+            let selectedEvent = categoryItems[indexPath.row].name
+            eventDetailViewController.category = selectedEvent
+            
+            
+        default:
+            fatalError("Unexpected Segue Identifier; \(String(describing: segue.identifier))")
+        }
+    }
 }
 
